4241ead948115f7931b39d739b3b3a43605e33e1
Enyewashebr <Enyewashebir0@gmail.com>
2025-08-04 21:13:58 +0300

    Refactor Hero component for improved readability and formatting consistency

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { words } from '../constants'
+import React from "react";
+import { words } from "../constants";
 
 const Hero = () => {
   return (
@@ -13,15 +13,24 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-              <h1>Shaping 
-                <span className='wrapper'>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
                     {words.map((word) => (
-                      <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                        <img 
-                        src={word.imgPath} alt={word.text}
-                        className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50' />
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
                         <span>{word.text}</span>
-                        </span>))}
+                      </span>
+                    ))}
+                  </span>
                 </span>
               </h1>
               <h1>into Real Projects</h1>
@@ -33,6 +42,6 @@ const Hero = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Hero
+export default Hero;
