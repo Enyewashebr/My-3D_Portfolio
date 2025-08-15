61a98766d2a69e2809f293ccc3470f91515fd9dc
Enyewashebr <Enyewashebir0@gmail.com>
2025-08-15 18:13:34 +0300

    Add Room component with 3D model and text elements
    - Introduced a new Room component that utilizes GLTF models and textures.
    - Integrated multiple mesh elements representing furniture and decor.
    - Added text elements to enhance the user experience with welcoming messages.
    - Included a picture frame with a texture for visual appeal.
    - Preloaded the optimized room GLB model for performance improvements.

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
+// import React, { use } from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { useMediaQuery } from 'react-responsive';
+import { Room } from './Room';
 // import { useLoader } from "@react-three/fiber";
 // import * as THREE from "three";
 
 const HeroExperience = () => {
+  const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+  // const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   // const texture = useLoader(THREE.TextureLoader, "/pic-logo.jpg");
 
     // const materials = [
     //   new THREE.MeshStandardMaterial({ map: texture }), // face 1 with image
-    //   new THREE.MeshStandardMaterial({ color: "teal" }), // face 2
-    //   new THREE.MeshStandardMaterial({ color: "teal" }), // face 3
-    //   new THREE.MeshStandardMaterial({ color: "teal" }), // face 4
-    //   new THREE.MeshStandardMaterial({ color: "teal" }), // face 5
-    //   new THREE.MeshStandardMaterial({ color: "teal" }), // face 6
+    //   // new THREE.MeshStandardMaterial({ color: "teal" }), // face 2
+    //   // new THREE.MeshStandardMaterial({ color: "teal" }), // face 3
+    //   // new THREE.MeshStandardMaterial({ color: "teal" }), // face 4
+    //   // new THREE.MeshStandardMaterial({ color: "teal" }), // face 5
+    //   // new THREE.MeshStandardMaterial({ color: "teal" }), // face 6
     // ];
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="1a1a40" />
       <directionalLight position={[5, 5, 5]} intensity={5} />
 
-      <OrbitControls />
+      <OrbitControls 
+      enablePan={false}
+      enableZoom={!isTablet}
+      maxDistance={20}
+      minDistance={5}
+      minPolarAngle={Math.PI / 5}
+      maxPolarAngle={Math.PI / 2}
+      />
 
       {/* <mesh material={materials} > */}
-      <mesh>
-        <boxGeometry args={[5, 5, 5]} />
+      {/* <mesh> */}
+        {/* <boxGeometry args={[5, 5, 5]} />
         {/* <sphereGeometry args={[5, 32, 32]} /> */}
         {/* <img src="/pic-logo.jpg" alt="" /> */}
-        <meshStandardMaterial color="teal" />
-      </mesh>
+         {/* <meshStandardMaterial color="teal" /> */}
+      {/* </mesh>  */} 
+
+      <Room />
+      
     </Canvas>
   );
 }
---
+++ src/components/HeroModels/Room.jsx
@@ -0,0 +1,207 @@
+
+import React from 'react'
+import { useGLTF, useTexture, Text } from "@react-three/drei";
+
+export function Room(props) {
+  const { nodes, materials } = useGLTF('/models/optimized-room.glb')
+  const myImageTexture = useTexture("/pic-logo.jpg");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes._________6_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.body1_blinn1_0.geometry}
+        material={materials.blinn1}
+      /> 
+      <Text
+        position={[0.5, 0.3, 2]} // X, Y, Z position in the room
+        fontSize={0.4} // Size of the text
+        color="red" // Text color
+        rotation={[-Math.PI / 2, 0, 0]}
+        // Optional: rotate if needed 0]} // Optional: rotate if needed
+      >
+        Welcome to My World!
+      </Text>
+
+      <Text
+        position={[0, 5.68, -3.3]} 
+        fontSize={0.4} 
+        color="blue" 
+        rotation={[-Math.PI / 2, 0, 0]}
+         // Optional: rotate if needed
+      >
+        Software Engineer!
+      </Text>
+      <Text
+        position={[-3.25, 5.68, -0.3]} // X, Y, Z position in the room
+        fontSize={0.4} // Size of the text
+        color="violet" // Text color
+        rotation={[-Math.PI / 2, 0, 1.57]}
+        // Optional: rotate if needed 0]} // Optional: rotate if needed
+      >
+        Design. Create. Inspire.!
+      </Text>
+      {/* <mesh geometry={nodes.body1_blinn1_0.geometry}>
+        <meshStandardMaterial map={myImageTexture} />
+      </mesh> */}
+      <group position={[0, 4, -2.9]}>
+        {/* Picture */}
+        <mesh>
+          <planeGeometry args={[2.5, 2.5]} />
+          <meshStandardMaterial map={myImageTexture} />
+        </mesh>
+
+        
+        {/** Frame */}
+        {/** Top frame */}
+        <mesh position={[0, 1.34, -0.05]}>
+          <boxGeometry args={[2.6, 0.1, 0.05]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/** Bottom frame */}
+        <mesh position={[0, -1.34, -0.05]}>
+          <boxGeometry args={[2.6, 0.1, 0.05]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/** Left frame */}
+        <mesh position={[-1.34, 0, -0.05]}>
+          <boxGeometry args={[0.1, 2.6, 0.05]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/** Right frame */}
+        <mesh position={[1.34, 0, -0.05]}>
+          <boxGeometry args={[0.1, 2.6, 0.05]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+      </group>
+
+      <mesh
+        geometry={nodes.cabin_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.chair_body_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.comp_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.emis_lambert1_0.geometry}
+        material={materials.lambert1}
+      />
+      <mesh
+        geometry={nodes.handls_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.keyboard_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.kovrik_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.lamp_bl_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.lamp_white_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.miuse_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.monitor2_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.monitor3_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.pCylinder5_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.pillows_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.polySurface53_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.radiator_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.radiator_blinn1_0001.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.railing_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.red_bttns_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.red_vac_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.stylus_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.table_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.tablet_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.triangle_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.vac_black_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.vacuum1_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.vacuumgrey_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.vires_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.window_blinn1_0.geometry}
+        material={materials.blinn1}
+      />
+      <mesh
+        geometry={nodes.window4_phong1_0.geometry}
+        material={materials.phong1}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload('/models/optimized-room.glb')
