7fbd957ae47191d78be040dffcfcea87f661f40b
Enyewashebr <Enyewashebir0@gmail.com>
2025-08-24 04:33:08 +0300

    added navbar components and its different functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from './Sections/Hero'
 import ShowcaseSection from './Sections/ShowcaseSection'
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import NavBar from './components/NavBar';
 
 
 const App = () => {
   return (
     <>
+    <NavBar />
     <Hero />
     <ShowcaseSection />
     </>
