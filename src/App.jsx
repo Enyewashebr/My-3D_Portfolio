f14894284ca92e663d9437011e4a6a2be02fb39a
Enyewashebr <Enyewashebir0@gmail.com>
2025-08-26 23:45:10 +0300

    feat: add EducationSection and TitleHeader components to enhance portfolio structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import NavBar from './components/NavBar';
 import LogoSection from './Sections/LogoSection';
 import FeatureCards from './Sections/FeatureCards';
+import EducationSection from './Sections/EducationSection';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
     <ShowcaseSection />
     <LogoSection />
     <FeatureCards />
+    <EducationSection />
     </>
   )
 }
---
+++ src/Sections/EducationSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+import { expCards } from '../constants'
+
+const EducationSection = () => {
+  return (
+    <section id='education' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
+      <div className='w-full h-full  md:px-20 px-5'>
+        <TitleHeader title=" Professional Education and Experience " sub="My Journey Overview"/>
+        <div className='mt-32 relative'>
+            <div className='relative z-50 xl:space-y-32 space-y-10 '>{expCards.map((card) =>(
+                <div key={card.title} className='exp-card-wrapper'>{card.title}</div>
+            ))}</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EducationSection
