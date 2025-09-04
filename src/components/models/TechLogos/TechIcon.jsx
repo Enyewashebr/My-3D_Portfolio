25b76dbc9822fbe5f17c3e946112083cd52c54d0
Enyewashebr <Enyewashebir0@gmail.com>
2025-09-05 00:12:59 +0300

    implement TechIcon component with 3D model rendering and environment setup

--- src/components/models/TechLogos/TechIcon.jsx
+++ src/components/models/TechLogos/TechIcon.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {useGLTF} from '@react-three/drei'
 
 const TechIcon = () => {
+    const scene = useGLTF(model.modelaPath)
   return (
-    <div>
-      
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.3} />
+      <Environment preset = 'city' />
+
+      <float>
+        <group>
+            <primitive object={scene.scene} />
+
+        </group>
+      </float>
+    </Canvas>
   )
 }
 
